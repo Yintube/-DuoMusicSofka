530390b7f773f82d2bf864d992f8c445b4fc1bb4
maohv14 <mauro1040@gmail.com>
2022-02-16 10:26:05 -0500

    Actualización clase FilterSong
    Se carga actualización de la clase FilterSong

--- BestPracticesPOO/src/classes/FilterSong.java
+++ BestPracticesPOO/src/classes/FilterSong.java
@@ -45,7 +45,7 @@ public class FilterSong extends MusicLibrary implements IPlayer {
                             System.out.println("    1. Rock.");
                             System.out.println("    2. Salsa.");
                             System.out.println("    3. Boleros y Tango.");
-                            System.out.println("    3. Balada Romantica.");
+                            System.out.println("    4. Balada Romantica.");
                             System.out.println("    5. Menú anterior");
                             System.out.println("");
                             option2 = input.nextInt();
@@ -86,6 +86,32 @@ public class FilterSong extends MusicLibrary implements IPlayer {
                     } while (flag2 != 2);
                     break;
                 case 2:
+                    int flag3 = 0;
+                    int option3 = 0;
+                    do {
+                        do {
+                            System.out.println("");
+                            System.out.println("Por favor ingrese el año que desea filtrar o 1 para regresar al menú anterior");
+                            System.out.println("");
+                            System.out.println("    1. Menú anterior");
+                            System.out.println("");
+                            option3 = input.nextInt();
+
+                            if (option3 > 1 && option3 <= 2022) {
+                                flag3 = 1;
+                                setYear2(option3);
+                                YearFilter(songs);
+                            } else if (option3 == 1) {
+                                flag3 = 2;
+                            } else {
+                                System.out.println("--------------------------------");
+                                System.out.println("No disponible, vuelva a intentar");
+                                System.out.println("--------------------------------");
+                            }
+
+                        } while (flag3 == 0);
+
+                    } while (flag3 != 2);
 
                     break;
                 case 3:
@@ -106,6 +132,7 @@ public class FilterSong extends MusicLibrary implements IPlayer {
      * @param songs
      */
     public void GenreFilter(Songs[] songs) {
+        int optionPlay = 0;
         System.out.println("Lista de canciones del Genero " + getGenre2());
         for (int i = 0; i < songs.length; i++) {
             if (songs[i].getGenre().equals(getGenre2())) {
@@ -113,8 +140,14 @@ public class FilterSong extends MusicLibrary implements IPlayer {
             }
 
         }
-    }
+        System.out.println("");
+        System.out.println("Desea reproducir Lista, para SI marque 1 para NO marque 0");
+        optionPlay = input.nextInt();
 
+        if (optionPlay == 1) {
+            toPlay();
+        }
+    }
     /**
      * Este método permite recorrer el arreglo de la lista de canciones y
      * mostrar en consola la lista acorde al año indicado por el usuario
@@ -122,41 +155,46 @@ public class FilterSong extends MusicLibrary implements IPlayer {
      * @param songs
      */
     public void YearFilter(Songs[] songs) {
-        System.out.println("Lista de canciones del año " + getGenre2() + " son:");
+        System.out.println("Lista de canciones del año " + getYear2() + " son:");
         int songCounter = 0;
+        int optionPlay = 0;
         for (int i = 0; i < songs.length; i++) {
             if (songs[i].getYear() == getYear2()) {
                 songCounter++;
-                System.out.println(songs[i].getIdentifier() + ". " + songs[i].getTitle());
+                System.out.println(songs[i].getIdentifier() + ". " + songs[i].getTitle() + "    " + songs[i].getDuration() + "Seg.");
             }
 
         }
         if (songCounter == 0) {
             System.out.println("No hay canciones en la biblioteca para ese año");
+        } else {
+            System.out.println("");
+            System.out.println("Desea reproducir Lista, para SI marque 1 para NO marque 0");
+            optionPlay = input.nextInt();
+
+            if (optionPlay == 1) {
+                toPlay();
+            }
         }
     }
-
     /**
      * @return the genre
      */
     public String getGenre2() {
         return genre2;
     }
-
     /**
      * @param genre the genre to set
      */
     public void setGenre2(String genre) {
         this.genre2 = genre;
     }
-
     /**
      * @return the year2
      */
     public int getYear2() {
         return year2;
     }
-
     /**
      * @param year2 the year2 to set
      */
@@ -166,11 +204,13 @@ public class FilterSong extends MusicLibrary implements IPlayer {
 
     @Override
     public void toPlay() {
+        System.out.println("Reproduciendo música");
 
     }
 
     @Override
     public void toStop() {
+        System.out.println("La reprodución se ha detenido");
 
     }
 
--- BestPracticesPOO/src/classes/Main.java
+++ BestPracticesPOO/src/classes/Main.java
@@ -2,7 +2,7 @@ package classes;
 
 /**
  *
- * @author yenifer
+ * @author 
  */
 public class Main {
 
